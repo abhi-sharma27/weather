5cfbc9cf6ff9ccce264c5ec355686d764193b769
abhi-sharma27 <abhishekpurika@gmail.com>
2020-12-17 19:15:52 +0530

    Basic Complete

--- src/core/homepage.js
+++ src/core/homepage.js
@@ -1,12 +1,16 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
+import classNames from "classnames";
 import "./homepage.css"
+import { FaSearchLocation } from "react-icons/fa";
 
-function Homepage () {
+function Homepage() {
 
-    let cityName="chandigarh";
+    let cityName = "chandigarh";
     const [data, setData] = useState(null);
+    const [yesno, setyesno] = useState(true);
 
-     function handleChange(value) {
+
+    function handleChange(value) {
         cityName = value.target.value;
     }
 
@@ -22,7 +26,7 @@ function Homepage () {
     //     });
     // }
 
-    async function handleSubmit (value) {
+    async function handleSubmit(value) {
         try {
             let data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=0898b95af8fb138034a326a18ecc3518`);
             data = await data.json();
@@ -34,15 +38,61 @@ function Homepage () {
         console.log('done');
     }
 
-    return(
-        <div>
-            <input type="text" placeholder="Enter City Name.." className="searchbar" onChange={handleChange}></input>
-            <button type="submit" onClick={handleSubmit}>Find weather</button>
+    
+
+    return (
+        <div className="homepage">
+
+            <div id="background-wrap">
+
+                <div id="sun"></div>
+
+                {/* <div class="x1">
+                    <div class="cloud"></div>
+                </div> */}
+
+                {/* <div class="x2">
+                    <div class="cloud"></div>
+                </div> */}
+
+                <div className="x3">
+                    <div className="cloud"></div>
+                </div>
+
+                <div className="x4">
+                    <div className="cloud"></div>
+                </div>
+
+                {/* <div class="x5">
+                    <div class="cloud"></div>
+                </div> */}
+            </div>
+  
+
+
+            <input type="text" placeholder="Enter City Name.." className={classNames ("searchbar" , {light:yesno}, {dark:!yesno} )} onChange={handleChange}></input>
+            {/* <button type="submit" className="submit-button" onClick={handleSubmit}>Find weather</button> */}
+            <FaSearchLocation className="submit" onClick={handleSubmit} />
+            <button onClick={()=> setyesno(!yesno)}>Change Border color</button>
+            
 
             <div>
-                <p>{data?.weather[0].description}</p>
-                <p>{data?.main.temp && "Temp : "+ (data?.main.temp-273.15)} </p>
-                <p>{data?.main.temp && "Sea Level : "+ (data?.main.sea_level)} </p>
+                
+                
+                {data?.main.temp && 
+                    <div className="details">
+                        <p>{data.name}</p>
+                        <p>{data?.weather[0].description}</p>
+                        <p>Temp :<span> {Math.round(data.main.temp - 273.15)}  &deg;C</span></p>
+                        <p>Feels Like :<span> {Math.round(data.main.feels_like - 273.15)}  &deg;C</span></p>
+                        <p>Wind :<span> {data.wind.speed} Km</span></p>
+                        <p>Sea Level :<span> {data.main.sea_level} m </span></p>
+                        <p>Visibility :<span> {(data.visibility/1000).toFixed(0)} Km </span></p>
+                        <p>SunRise :<span> {(new Date(data.sys.sunrise*1000)).toLocaleTimeString()}</span></p>
+                        <p>Sunset :<span>{(new Date(data.sys.sunset*1000)).toLocaleTimeString()}</span></p>
+                    </div>
+                }
+                
             </div>
         </div>
     );
